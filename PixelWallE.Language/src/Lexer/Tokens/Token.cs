908aa68bacf617401d13bca04fbc596c49cebe8b
Javier Fontes <javierfontbas@gmail.com>
2025-04-14 10:25:41 -0400

    Add Token class Structure

--- PixelWallE.Language/src/Lexer/Tokens/Token.cs
+++ PixelWallE.Language/src/Lexer/Tokens/Token.cs
@@ -1,6 +1,23 @@
-﻿namespace PixelWallE.Language.Tokens;
+﻿using System.Dynamic;
+
+namespace PixelWallE.Language.Tokens;
 
 public class Token
 {
-    
+     public TokenType Type { get; private set;}
+     public string Value {get; private set;}
+     public CodeLocation Location {get; private set;}
+
+     public Token(TokenType type, string value, CodeLocation location){
+        Type=type;
+        Value=value;
+        Location=location;
+     }
+
+     //View Token
+     public String toString(){
+        return Type+ " "+ Value;
+     }
+
 }
+
