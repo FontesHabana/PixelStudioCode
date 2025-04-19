8b613566b60758c575b57e08dcec4af0b12780f7
Javier Fontes <javierfontbas@gmail.com>
2025-04-19 16:30:53 -0400

    Add ParenthesizedExpression class implementation

---
+++ PixelWallE.Language/src/Expressions/ParenthesizedExpression/ParenthesizedExpression.cs
@@ -0,0 +1,32 @@
+﻿
+namespace PixelWallE.Language.Parsing.Expressions
+;
+
+public class ParenthesizedExpression : Expression
+{
+    public Expression InnerExpression { get; set; }
+    public override ExpressionType Type{get;set;}
+
+    public override object? Value { get; set; }
+
+    public ParenthesizedExpression(CodeLocation location, Expression innerExpression) : base(location)
+    {
+        InnerExpression = innerExpression;
+    }
+    public override bool CheckSemantic(Context context, Scope scope, List<CompilingError> errors)
+    {
+        bool check=InnerExpression.CheckSemantic(context,scope,errors);
+        Type= InnerExpression.Type;
+        return check;
+    }
+
+    public override void Evaluate()
+    {
+        InnerExpression.Evaluate();
+        Value=InnerExpression.Value;
+       
+    }
+
+
+
+}
