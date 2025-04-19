ac162d57f818a9c0e4f5df824bf265d8df1fdaf1
Javier Fontes <javierfontbas@gmail.com>
2025-04-19 17:11:35 -0400

    Add OrOperation class implementation for logical disjunction evaluation

---
+++ PixelWallE.Language/src/Expressions/BinaryExpression/LogicOperation/OROperation.cs
@@ -0,0 +1,41 @@
+namespace PixelWallE.Language.Parsing.Expressions;
+
+public abstract class OrOperation: BinaryExpression
+{
+    public override ExpressionType Type {get; set;}
+    public override object? Value {get; set;}
+
+    public OrOperation(CodeLocation location) : base(location){}
+
+    public override void Evaluate()
+    {
+        Right.Evaluate();
+        Left.Evaluate();
+        //Revisar ma¡ás adelante división por cero
+        Value = (bool)Left.Value || (bool)Right.Value;
+    }
+
+    public override bool CheckSemantic(Context context, Scope scope, List<CompilingError> errors)
+    {
+        bool right = Right.CheckSemantic(context, scope, errors);
+        bool left = Left.CheckSemantic(context, scope, errors);
+        if (Right.Type != ExpressionType.Bool || Left.Type != ExpressionType.Bool)
+        {
+            errors.Add(new CompilingError(Location, ErrorCode.Invalid, "Este texto de error hay que cambiarlo We don't do that here... "));
+            Type = ExpressionType.ErrorType;
+            return false;
+        }
+
+        Type = ExpressionType.Bool;
+        return right && left;
+    }
+
+    public override string ToString()
+    {
+        if (Value == null)
+        {
+            return String.Format("({0} || {1})", Left, Right);
+        }
+        return Value.ToString();
+    }
+}
