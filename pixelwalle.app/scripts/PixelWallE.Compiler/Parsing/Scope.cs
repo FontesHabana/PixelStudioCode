7a3611b3230277948e049c8c69d22346a58872b4
Javier Fontes <javierfontbas@gmail.com>
2025-05-24 19:01:26 -0400

    Refactor Scope class to improve variable and color management, enhancing readability and maintainability.

--- pixelwalle.app/scripts/PixelWallE.Compiler/Parsing/Scope.cs
+++ pixelwalle.app/scripts/PixelWallE.Compiler/Parsing/Scope.cs
@@ -1,92 +1,124 @@
 using PixelWallE.Language.Parsing.Expressions;
 using System.Collections.Generic;
+using System.Drawing;
+
 
 namespace PixelWallE.Language.Parsing;
 
- public class Scope
-    {   //Permite extender el lenguaje a contextos globales y bloques de codigo
-        public readonly Dictionary<string,(object, ExpressionType)> variables=new();
-        public readonly Dictionary<string,Label> labels=new();
-        //
-        private readonly Scope? parent;
-        //Se pueden agregar las funciones aquí;
-        public Scope(  List<Label> labels,Scope? parent=null){
-            
-            this.parent=parent;
-            foreach (var item in labels)
-            {
-                this.labels.Add(item.LabelReference,item);
-            }
+public class Scope
+{   //Permite extender el lenguaje a contextos globales y bloques de codigo
+    public readonly Dictionary<string, (object, ExpressionType)> variables = new();
+    public readonly Dictionary<string, Label> labels = new();
+
+    public readonly Dictionary<string, Godot.Color> colors = new();
+    //
+    private readonly Scope? parent;
+    //Se pueden agregar las funciones aquí;
+    public Scope(List<Label> labels, Scope? parent = null)
+    {
+
+        this.parent = parent;
+        foreach (var item in labels)
+        {
+            this.labels.Add(item.LabelReference, item);
+        }
+        colors = new()
+        {
+             {"Transparent", new  Godot.Color(255, 255, 255, 0)},
+            {"Red", new  Godot.Color(255, 0, 0)},
+            {"Blue", new  Godot.Color(0, 0, 255)},
+            {"Green", new  Godot.Color(0, 255, 0)},
+            {"Yellow", new  Godot.Color(255, 255, 0)},
+            {"Orange", new  Godot.Color(255, 165, 0)},
+            {"Purple", new  Godot.Color(160, 32, 240)},
+            {"Black", new  Godot.Color(0, 0, 0)},
+            {"White", new  Godot.Color(255, 255, 255)},
+            {"Pink", new  Godot.Color(255, 80, 220)},
+        };
+    }
+
+
+    public bool IsDeclared<T>(string name, Dictionary<string, T> dic)
+    {
+        if (dic.ContainsKey(name))
+            return true;
+        else if (parent != null)
+        {
+            return parent.IsDeclared(name, dic);
+        }
+        return false;
+    }
+    public bool DeclareVariable(string name, object value, ExpressionType type)
+    {
+        if (variables.ContainsKey(name))
+        {
+            return false;
+        }
+        variables[name] = (value, type);
+        return true;
+    }
+
+    public bool AssignVariable(string name, object value, ExpressionType type)
+    {
+        if (variables.ContainsKey(name))
+        {
+            variables[name] = (value, type);
+            return true;
+        }
+        else if (parent != null)
+        {
+            return parent.AssignVariable(name, value, type);
         }
 
-        public bool DeclareVariable(string name, object value, ExpressionType type){
-            if (variables.ContainsKey(name))
-            {
-                return false;
-            }
-                variables[name]=(value,type);
-                return true;
-        }
-    
-        public bool AssignVariable(string name, object value, ExpressionType type){
-            if (variables.ContainsKey(name))
-            {   variables[name]=(value,type);
-                return true;
-            }else if(parent!=null){
-                return parent.AssignVariable(name, value, type);
-            }
-           
-            return false;
-        }
+        return false;
+    }
 
-        public object? GetVariable(string name){
-            if (variables.ContainsKey(name)) 
-            {
-                return variables[name].Item1;
-            }
-            return parent?.GetVariable(name);
+    public object? GetVariable(string name)
+    {
+        if (variables.ContainsKey(name))
+        {
+            return variables[name].Item1;
         }
-        
-        public ExpressionType GetVariableType(string name){
-            if (variables.ContainsKey(name)) 
-            {
-                return variables[name].Item2;
-            }
-            if (parent==null)
-            {
-                return ExpressionType.Anytype;
-            }
-            return parent.GetVariableType(name);
+        return parent?.GetVariable(name);
+    }
+
+    public ExpressionType GetVariableType(string name)
+    {
+        if (variables.ContainsKey(name))
+        {
+            return variables[name].Item2;
         }
-        public bool IsDeclaredVariable(string name){
-            if (variables.ContainsKey(name)) 
-                return true;
-            else if(parent!=null)
-            {
-                return parent.IsDeclaredVariable(name);
-            }
-            return false;
+        if (parent == null)
+        {
+            return ExpressionType.Anytype;
         }
-                
-        
-         public bool IsDeclaredLabel(string name){
-            if (labels.ContainsKey(name)) 
-                return true;
-             else if(parent!=null)
-            {
-                return parent.IsDeclaredLabel(name);
-            }
-            return false;
+        return parent.GetVariableType(name);
+    }
+
+
+
+
+    public Label? GetLabel(string name)
+    {
+        if (labels.ContainsKey(name))
+        {
+            return labels[name];
         }
-                
-        public Label? GetLabel(string name){
-            if (labels.ContainsKey(name)) 
-            {
-                return labels[name];
-            }
-            return parent?.GetLabel(name);
+        return parent?.GetLabel(name);
+    }
+
+
+public Godot.Color? GetColor(string name)
+    {
+        if (colors.ContainsKey(name))
+        {
+            return colors[name];
         }
-        
-         //Aqui hay DRY revisa
-                
-        }
+        return parent?.GetColor(name);
+    }
+
+    //Aqui hay DRY revisa
+
+
+
+}
